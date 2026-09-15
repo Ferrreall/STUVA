1a4e783ac01332e3d6890a53ca77c91d1ce6305b
Vahllzzzz <www.rehanwangsaff@gmail.com>
2026-09-15 16:31:46 +0700

    fix name

--- STUVA-FE/src/main.js
+++ STUVA-FE/src/main.js
@@ -5,7 +5,7 @@ import router from './router'
 import { createPinia } from 'pinia'
 import Toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
-import './assets/css/Variables.css'
+import './assets/css/variables.css'
 import './style.css'
 
 
